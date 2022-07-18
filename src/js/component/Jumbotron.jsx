aa1cb27f5ccd50e61bd1bf9854aa9ca44e540512
flaviapl <flavia_pala@hotmail.it>
2022-07-18 19:12:01 +0000

    add first Jumbo

---
+++ src/js/component/Jumbotron.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Jumbotron = () => {
+  return (
+    <div className="card">
+      <div className="card-header">Featured</div>
+      <div className="card-body">
+        <h5 className="card-title">A Warm Welcome!</h5>
+        <p className="card-text">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est quas, sit sapiente incidunt laudantium veritatis saepe quisquam recusandae dolores maxime nostrum necessitatibus sed possimus? Impedit commodi ipsam quasi sequi distinctio.
+        </p>
+        <a href="#" className="btn btn-primary">
+          Call to action!
+        </a>
+      </div>
+    </div>
+  );
+};
+export default Jumbotron
