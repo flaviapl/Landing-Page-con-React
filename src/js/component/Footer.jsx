e2a6f88b3df783904589cb4e03ee9e3200e61220
Moyra_sc <noemisolac@gmail.com>
2022-07-18 21:21:08 +0000

    create footer

---
+++ src/js/component/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="card-footer text-muted text-center bg-dark text-white">
+      Copyright &copy; Flavia, Carmen y Noe 2022
+    </div>
+  );
+};
+
+export default Footer;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import Jumbotron from "./Jumbotron.jsx";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 import NavBar from "./Navbar.jsx";
+import Footer from "./Footer.jsx";
 
 //create your first component
 const Home = () => {
   return (
     <>
-      <Jumbotron />
       <NavBar />
+      <Jumbotron />
+      <Footer />
+      
     </>
   );
 };
