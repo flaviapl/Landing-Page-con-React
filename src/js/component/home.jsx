83b7e0143e903803e1780a224a647d28d82bda99
Moyra_sc <noemisolac@gmail.com>
2022-07-18 21:47:39 +0000

    estilos navbar y footer

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,16 +2,17 @@ import React from "react";
 import Jumbotron from "./Jumbotron.jsx";
 import NavBar from "./Navbar.jsx";
 import Footer from "./Footer.jsx";
+import "../../styles/index.css"
 
 //create your first component
 const Home = () => {
   return (
-    <>
+    <div className="container">
       <NavBar />
       <Jumbotron />
       <Footer />
       
-    </>
+    </div>
   );
 };
 
