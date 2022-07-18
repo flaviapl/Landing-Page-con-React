ef55ef197c4f9b3e5465a53c99be6afc431193bd
Moyra_sc <noemisolac@gmail.com>
2022-07-18 22:57:54 +0000

    navbar colapsable,modificación de footer y estilos

--- src/js/component/Footer.jsx
+++ src/js/component/Footer.jsx
@@ -5,6 +5,7 @@ const Footer = () => {
     <div className="card-footer text-muted text-center bg-dark text-white">
       Copyright &copy; Flavia, Carmen y Noe 2022
     </div>
+
   );
 };
 
--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -2,31 +2,53 @@ import React from "react";
 
 const NavBar = () => {
   return (
-    <nav className="navbar navbar-dark bg-dark d-flex justify-content-around">
-			<span className="navbar-brand mb-0 h1"> Start Bootstrap </span>
+    <div className=" navbar-dark bg-dark ">
+      <nav className="navbar navbar-expand-md  ">
+        <div className="container-fluid">
+          <a href="#" className="navbar-brand">
+            Start Bootstrap
+          </a>
+          <button
+            type="button"
+            className="navbar-toggler"
+            data-bs-toggle="collapse"
+            data-bs-target="#MenuNavegacion"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+        </div>
 
-			<ul className="nav">
-				<li className="nav-item">
-					<a className="nav-link active" aria-current="page" href="#">
-						Home
-					</a>
-				</li>
-				<li className="nav-item">
-					<a className="nav-link" href="#">
-						About
-					</a>
-				</li>
-				<li className="nav-item">
-					<a className="nav-link" href="#">
-						Services
-					</a>
-				</li>
-				<li className="nav-item">
-					<a className="nav-link">Contact</a>
-				</li>
-			</ul>
-		</nav>
+        <div id="MenuNavegacion" className="collapse navbar-collapse">
+          <ul className="navbar-nav ms-3">
+            <li className="nav-item">
+              <a className="nav-link active" href="#">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Services
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link "
+                href="#"
+               
+              >
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
   );
 };
 
-export default NavBar
+export default NavBar;
