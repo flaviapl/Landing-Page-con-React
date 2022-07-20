3cefa76340eb0640e3f3618628035e202592a9d4
flaviapl <flavia_pala@hotmail.it>
2022-07-20 12:16:45 +0000

    cambio a jumbotron y add div y margin a Jumbotron y card 1-2-3-4

--- src/js/component/Jumbotron.jsx
+++ src/js/component/Jumbotron.jsx
@@ -2,14 +2,13 @@ import React from "react";
 
 const Jumbotron = () => {
   return (
-    <div className="card">
-      <div className="card-header">Featured</div>
+    <div className="cardd">
       <div className="card-body">
-        <h5 className="card-title">A Warm Welcome!</h5>
+        <h2 className="card-title">A Warm Welcome!</h2>
         <p className="card-text">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est quas, sit sapiente incidunt laudantium veritatis saepe quisquam recusandae dolores maxime nostrum necessitatibus sed possimus? Impedit commodi ipsam quasi sequi distinctio.
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est quas, sit sapiente incidunt laudantium veritatis saepe quisquam recusandae dolores maxime nostrum necessitatibus sed possimus? Impedit commodi ipsam quasi sequi distinctio. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti aspernatur.
         </p>
-        <a href="#" className="btn btn-primary">
+        <a href="#" className="btn btn-primary p">
           Call to action!
         </a>
       </div>
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,23 +3,23 @@ import Jumbotron from "./Jumbotron.jsx";
 import Card from "./card.jsx";
 import NavBar from "./Navbar.jsx";
 import Footer from "./Footer.jsx";
-import "../../styles/index.css"
+import "../../styles/index.css";
 
 //create your first component
 const Home = () => {
   return (
     <>
       <NavBar />
-      <Jumbotron />
-      <Card/>
-		  <Card/>
-		  <Card/>
-		  <Card/>
+      <div className="container1">
+        <Jumbotron />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
       <Footer />
-      
     </>
   );
-  
 };
 
 export default Home;
