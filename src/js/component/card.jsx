22d3f0deed675bf43ea7f1990ec6842a685da386
Moyra_sc <noemisolac@gmail.com>
2022-07-24 11:32:01 +0000

    style: ajusto ancho img

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 const Card = () => {
-    return (<div className="card cards col-xl-3 col-xs-12 ps-0 pe-4" >
+    return (<div className="card cards col-xl-3 col-xs-12 ps-0 " >
     <img src="https://www.precisemedical.co.uk/wp-content/uploads/2017/05/grey-square.png" className="card-img-top" alt="..."/>
     <div className="card-body">
       <h5 className="card-title">Card title</h5>
